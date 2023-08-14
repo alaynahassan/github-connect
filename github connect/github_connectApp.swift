48e27311a521815e6b6329896e975ce71c9252f8
Alayna Hassan <alaynahassan08@gmail.com>
2023-08-14 16:19:42 +0200

    Initial Commit

---
+++ github connect/github_connectApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_connectApp.swift
+//  github connect
+//
+//  Created by Alayna Hassan on 2023-08-14.
+//
+
+import SwiftUI
+
+@main
+struct github_connectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
